20ba04a5b2c2d04ff0fb0f96d651afe60779be63
Samridhi <shreesamridhi503@gmail.com>
2025-04-09 19:59:45 +0530

    Updates

---
+++ precache-manifest.e5b0201e92540d21024d3c3473399b29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1d5a92381d363a213718d7191dac0c",
+    "url": "/weather-dashboard/index.html"
+  },
+  {
+    "revision": "8e507d9565df6f37170d",
+    "url": "/weather-dashboard/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "4c558f9fe7e800753428",
+    "url": "/weather-dashboard/static/js/2.5b3c5e6f.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/weather-dashboard/static/js/2.5b3c5e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099acb9786eec7a8aa03",
+    "url": "/weather-dashboard/static/js/3.88b3e670.chunk.js"
+  },
+  {
+    "revision": "8e507d9565df6f37170d",
+    "url": "/weather-dashboard/static/js/main.104ba070.chunk.js"
+  },
+  {
+    "revision": "f57fea7c979e5e30f095",
+    "url": "/weather-dashboard/static/js/runtime-main.a45762fa.js"
+  }
+]);
